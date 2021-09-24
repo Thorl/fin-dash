13a79c7495fcde13fbab779859aab3862176e740
Thorl <lofgrenthor@gmail.com>
2021-09-24 08:24:29 +0200

    Refactor(Search.jsx): Break out <input> to separate component
    - Broke out the input field in Search.jsx to its own component at
      src/features/Search/components/Input/SearchInput.jsx. The component
      handles passing the search query to fetchSymbolHandler, and updating
      the state of searchResults and isLoading, which are props passed
      down from Search.jsx.
    
    - Changed the <div> tag that wrapped the input element to a <form> tag
      to be more semantically correct.
    
    - Made the AbortController work properly by passing the
      AbortController.signal property down to the fetchStockSymbols function.
      Calling controller.abort() in the useEffect cleanup prevents
      unnecessary api requests being made if the user quickly changes their
      search query.
    
    - In fetch-symbol-handler: Added another condition in the filter method
      on mergedSearchResults to see if the current element is truthy, because
      there was an error being thrown of the type "Can't read length of
      undefined."

--- src/features/Search/Search.jsx
+++ src/features/Search/Search.jsx
@@ -1,68 +1,19 @@
-import { useRef, useEffect, useState } from "react";
+import { useState } from "react";
 
+import { SearchInput } from "./components/Input/SearchInput";
 import { Result } from "./components/Result/Result";
-import { fetchSymbolHandler } from "./api/alpha-vantage/fetch-symbol-handler";
 import * as styles from "./Search.module.css";
 
 export const Search = (props) => {
   const [searchResults, setSearchResults] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const focusSearch = useRef(null);
-
-  useEffect(() => {
-    focusSearch.current.focus();
-  });
-
-  const loadSymbols = (query) => {
-    return fetchSymbolHandler(query);
-  };
-
-  const sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-
-  useEffect(() => {
-    let currentQuery = true;
-    const controller = new AbortController();
-
-    const loadSearchResults = async () => {
-      if (!searchQuery) {
-        setIsLoading(false);
-        return setSearchResults([]);
-      }
-
-      setIsLoading(true);
-      setSearchResults([]);
-
-      await sleep(500);
-      if (currentQuery) {
-        const results = await loadSymbols(searchQuery, controller);
-        setSearchResults(results);
-        setIsLoading(false);
-      }
-    };
-
-    loadSearchResults();
-
-    return () => {
-      currentQuery = false;
-      controller.abort();
-    };
-  }, [searchQuery]);
 
   return (
     <>
-      <div className={styles["SearchField"]}>
-        <input
-          className={styles["SearchField__Input"]}
-          type="text"
-          placeholder="Enter your search for a stock ticker or company name, currency pair, or crypto/fiat currency pair."
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.target.value)}
-          ref={focusSearch}
-        ></input>
-      </div>
+      <SearchInput
+        onSetSearchResults={setSearchResults}
+        onSetIsLoading={setIsLoading}
+      />
       <div className={styles["SearchResultsGrid"]}>
         <div className={styles["SearchResultsHeader"]}>
           <div>Type</div>
