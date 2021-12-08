64b704b619391266a310e1193788ce92202d2ca0
Thorl <lofgrenthor@gmail.com>
2021-12-08 17:24:57 +0100

    Chore(Update how .env file is accessed)
    The process.env file was accessed incorrectly. Changed it to the correct dot notation.

--- .netlify/functions/api.js
+++ .netlify/functions/api.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      api: process.env.SERVER_API_KEY,
+      api: process.env.API_KEY,
     }),
   };
 };
